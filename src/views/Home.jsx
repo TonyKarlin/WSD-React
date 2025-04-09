eea25ec4bcdf60857664d2764f5433adb5b8ef24
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2025-04-09 11:18:27 +0300

    Update fetchData call in Home component to use environment variable for media API

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   useEffect(() => {
     const getMedia = async () => {
       try {
-        const data = await fetchData('test.json');
+        const data = await fetchData(import.meta.env.VITE_MEDIA_API + '/media');
         setMediaArray(data);
       } catch (e) {
         console.error('error', e);
